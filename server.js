cb8493d79094b7f64ef0451fd427e7e1247a7d7a
PradyumnVSCODE <pradyumn.lavania@hotmail.com>
2026-06-07 15:39:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-console.log("🔥 CLEAN FILE IS RUNNING");
+console.log("🔥 NODE IS RUNNING THIS FILE");
 
-import http from "http";
-
-http.createServer((req, res) => {
-  console.log("REQUEST:", req.url);
-  res.end("OK " + req.url);
-}).listen(3000, "127.0.0.1");
-
-console.log("LISTENING 3000");
+process.stdin.resume();
