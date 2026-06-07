bcecbe5f5244861c056e8177e29c42861b5f15b9
PradyumnVSCODE <pradyumn.lavania@hotmail.com>
2026-06-07 15:29:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import http from "http";
 
-http.createServer((req, res) => {
-  console.log("HIT:", req.url);
+console.log("🔥 FILE LOADED");
 
-  res.end("OK");
-}).listen(3000, "0.0.0.0");
+http.createServer((req, res) => {
+  console.log("🔥 REQUEST RECEIVED:", req.url);
+
+  res.end("OK " + req.url);
+}).listen(3000, "127.0.0.1", () => {
+  console.log("RUNNING ON 127.0.0.1:3000");
+});
