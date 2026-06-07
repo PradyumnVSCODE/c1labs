0bb3cc110c0da26611585d344671009062e399c1
PradyumnVSCODE <pradyumn.lavania@hotmail.com>
2026-06-07 15:43:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
-console.log("NODE ACTIVE");
-process.stdin.resume();
+import express from "express";
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  console.log("NEW SERVER HIT");
+  res.send("FRESH OK");
+});
+
+app.listen(3000, () => {
+  console.log("FRESH SERVER RUNNING");
+});
